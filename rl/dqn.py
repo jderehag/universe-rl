107c0f8afd247f02fafaec1e132de637dca009c8
Jesper Derehag <jderehag@hotmail.com>
2017-01-23 10:52:09 +0100

    dqn: Cleaned up for readability
    Signed-off-by: Jesper Derehag <jderehag@hotmail.com>

--- rl/dqn.py
+++ rl/dqn.py
@@ -66,70 +66,65 @@ class DQN(object):
         self._observe_state_length = observe_state_length
         self._observations_per_state = observations_per_state
 
+        self._replay_memory = RingBuffer(replay_memory_size, dtype=np.ndarray)
+
         # Counters
         self._timestep = 0
         self._prev_timestep = 0
         self._prev_timestamp = time.clock()
 
-        self._replay_memory = RingBuffer(replay_memory_size, dtype=np.ndarray)
-        self._current_state = None
+        # Models are created at first observation (lazy initialization)
         self._is_initialized = False
+        self._current_state = None
+        self._q_model = None
+        self._q_model_t = None
 
-    def _init_for_first_observation(self, observation):
-        self._is_initialized = True
-        self._current_state = np.stack((observation,) * self._observations_per_state, axis=-1)
-        self._init_convnets(observation)
-
-    def _init_convnets(self, observation):
-        # pylint: disable=attribute-defined-outside-init
-        input_dimension = list(observation.shape[:2]) + [self._observations_per_state]
-
-        self.q_model = Sequential()
-        self.q_model.add(Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same',
-                                       input_shape=input_dimension, dim_ordering='tf'))
-        self.q_model.add(Activation('relu'))
-
-        self.q_model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
-        self.q_model.add(Activation('relu'))
-        self.q_model.add(Dropout(0.25))
-
-        self.q_model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same'))
-        self.q_model.add(Activation('relu'))
-        self.q_model.add(Dropout(0.25))
-
-        self.q_model.add(Flatten())
-        self.q_model.add(Dense(512))
-        self.q_model.add(Activation('relu'))
-        self.q_model.add(Dropout(0.5))
-
-        self.q_model.add(Dense(self._actions))
-        self.q_model.add(Activation('linear'))
-
-        self.q_model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['accuracy'])
-
-        # Copy q_model into q_model_t
-        self.q_model_t = copy.copy(self.q_model)
-        self._copy_target_qnetwork()
-
-    def get_action(self, next_observation, action, reward, terminal):
+    def get_action(self, observation, action, reward, terminal):
         '''
-        TODO: write a docstring
-        '''
-        assert len(next_observation.shape) == 3 and \
-               next_observation.shape[2] == 1, 'Only single channel frames supported!'
+        Gets an action given an observation, action, reward, terminal
 
-        next_observation.shape = next_observation.shape[:2]
+        Essentially done in 4 steps:
+            1. Store all observations in a fifo queue (experience replay)
+            2. Trains a model to predict the reward using data from the fifo queue
+            3. Predict the expected reward for all possible actions given this observation
+            4. Do epsilon greedy selection of action (select random action given some probability epsilon)
+
+        Args:
+            observation (np.ndarray, shape(height,width,z=1)): The current observation (frame)
+            action (int): The action performed prior to the current observation
+            reward(float): The reward given this action and observation
+            terminal(Boolean): End of episode, i,e if the game terminated
+
+        Returns:
+            action(int): The selected action as an index into the list of possible actions
+        '''
+        observation = self._remove_channels(observation)
 
         if not self._is_initialized:
-            self._init_for_first_observation(next_observation)
+            self._init_for_first_observation(observation)
 
-        self._set_perception(next_observation, action, reward, terminal)
-        return self._get_action()
+        self._new_state(observation, action, reward, terminal)
+
+        if self.state != 'observe':
+            self._train()
+
+        action = self._epsilon_greedy_action()
+
+        self._timestep += 1
+        return action
 
     @property
-    def dqn_state(self):
+    def state(self):
         '''
-        TODO: Write a docstring
+        The overall state of the agent
+
+        The agent works in 3 phases:
+            1. observe (only populate replay memory)
+            2. explore (epilon-greedy annealation)
+            3. train (no more epsilon annealation, play and explore with _epsilon_final probability)
+
+        Returns:
+            state(str): observe|explore|train
         '''
         if self._timestep <= self._observe_state_length:
             return "observe"
@@ -139,38 +134,7 @@ class DQN(object):
         else:
             return 'train'
 
-    def _copy_target_qnetwork(self):
-        self.q_model_t.set_weights(self.q_model.get_weights())
-
-    def _set_perception(self, next_observation, action, reward, terminal):
-        # Copy current-state (excluding oldest frame)
-        new_state = np.append(self._current_state[:, :, 1:], np.expand_dims(next_observation, axis=-1), axis=-1)
-        self._replay_memory.append((self._current_state, action, reward, new_state, terminal))
-
-        if self.dqn_state != 'observe':
-            self._train_qnetwork()
-
-        self._current_state = new_state
-        self._timestep += 1
-
-    def _get_action(self):
-        qvalue = self.q_model.predict_on_batch(np.asarray([self._current_state]))[0]
-
-        # Assume action=0 means do nothing
-        action = 0
-        if self._timestep % self._frames_per_action == 0:
-            if random.random() <= self._epsilon:
-                action = random.randrange(self._actions)
-            else:
-                action = np.argmax(qvalue)
-
-        if self.dqn_state != 'observe' and self._epsilon > self._epsilon_final:
-            self._epsilon -= self._epsilon_anneal_over_frames
-
-        return action
-
-
-    def _train_qnetwork(self):
+    def _train(self):
         state_batch = []
         action_batch = []
         reward_batch = []
@@ -186,14 +150,81 @@ class DQN(object):
             terminal_batch.append(sample[4])
 
         # Calculate discounted future reward, and update with new action-reward
-        qvalue_t_batch = self.q_model_t.predict_on_batch(np.asarray(next_state_batch))
+        qvalue_t_batch = self._q_model_t.predict_on_batch(np.asarray(next_state_batch))
 
         for terminal, reward, action, qvalue_t in zip(terminal_batch, reward_batch, action_batch, qvalue_t_batch):
             if not terminal:
                 reward = reward + self._gamma * np.max(qvalue_t)
             qvalue_t[action] = reward
 
-        self.q_model.fit(np.asarray(state_batch), qvalue_t_batch, verbose=0, batch_size=self._batch_size)
+        self._q_model.fit(np.asarray(state_batch), qvalue_t_batch, verbose=0, batch_size=self._batch_size)
 
         if self._timestep % self._q_target_update_interval == 0:
             self._copy_target_qnetwork()
+
+    def _new_state(self, observation, action, reward, terminal):
+        # Copy current-state (excluding oldest frame) and append new observation at end
+        # Future: optimize by using np.roll() and no copy!
+        new_state = np.append(self._current_state[:, :, 1:], np.expand_dims(observation, axis=-1), axis=-1)
+        self._replay_memory.append((self._current_state, action, reward, new_state, terminal))
+        self._current_state = new_state
+
+    def _epsilon_greedy_action(self):
+        action = 0  # Assume action=0 means do nothing
+        if self._timestep % self._frames_per_action == 0:
+            if random.random() <= self._epsilon:
+                action = random.randrange(self._actions)
+            else:
+                qvalue = self._q_model.predict_on_batch(np.asarray([self._current_state]))[0]
+                action = np.argmax(qvalue)
+
+        # Update epsilon
+        if self.state == 'explore':
+            self._epsilon -= self._epsilon_anneal_over_frames
+
+        return action
+
+    def _init_for_first_observation(self, observation):
+        self._is_initialized = True
+        self._current_state = np.stack((observation,) * self._observations_per_state, axis=-1)
+        self._init_convnets(observation)
+
+    def _init_convnets(self, observation):
+        input_dimension = list(observation.shape[:2]) + [self._observations_per_state]
+
+        self._q_model = Sequential()
+        self._q_model.add(Convolution2D(32, 8, 8, subsample=(4, 4), border_mode='same',
+                                        input_shape=input_dimension, dim_ordering='tf'))
+        self._q_model.add(Activation('relu'))
+
+        self._q_model.add(Convolution2D(64, 4, 4, subsample=(2, 2)))
+        self._q_model.add(Activation('relu'))
+        self._q_model.add(Dropout(0.25))
+
+        self._q_model.add(Convolution2D(64, 3, 3, subsample=(1, 1), border_mode='same'))
+        self._q_model.add(Activation('relu'))
+        self._q_model.add(Dropout(0.25))
+
+        self._q_model.add(Flatten())
+        self._q_model.add(Dense(512))
+        self._q_model.add(Activation('relu'))
+        self._q_model.add(Dropout(0.5))
+
+        self._q_model.add(Dense(self._actions))
+        self._q_model.add(Activation('linear'))
+
+        self._q_model.compile(loss='mean_squared_error', optimizer='rmsprop', metrics=['accuracy'])
+
+        # Copy _q_model into _q_model_t
+        self._q_model_t = copy.copy(self._q_model)
+        self._copy_target_qnetwork()
+
+    def _copy_target_qnetwork(self):
+        self._q_model_t.set_weights(self._q_model.get_weights())
+
+    @staticmethod
+    def _remove_channels(observation):
+        assert len(observation.shape) == 3 and observation.shape[2] == 1, 'Only single channel frames supported!'
+        observation.shape = observation.shape[:2]
+        return observation
+
